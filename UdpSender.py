83b8b7ad7b2e4f47539460530ab6116c1050358a
Frederik Kristensen <fred009a@edu.easj.dk>
2020-12-04 10:20:08 +0100

    Udp sender

---
+++ UdpSender.py
@@ -0,0 +1,13 @@
+BROADCAST_TO_PORT = 7000
+import time
+from socket import *
+from datetime import datetime
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+while True:
+	data = "Id: 1, Name: Maskine 1, Temperature: 38.7, Location: Der " + str(datetime.now())
+	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+	print(data)
+	time.sleep(1)
